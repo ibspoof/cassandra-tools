e95b26117430605750f3244c2abcfbbb77d4d7ba
Brad Vernon <brad.vernon@datastax.com>
2016-04-29 12:02:05 -0700

    Adding echo's for easier notice of what is happening

--- upgrading/dse-4.6/upgrade_to_4.8.sh
+++ upgrading/dse-4.6/upgrade_to_4.8.sh
@@ -8,11 +8,13 @@ BACKUP_DIR=/etc/dse/cassandra/_backup_46
 SLEEP_TIME=5
 
 # backup current configuration
+echo "Backing up existing cassandra configs..."
 cp $DEFAULT_YAML_LOCATION $NEW_YAML
 mkdir $BACKUP_DIR
 cp /etc/dse/cassandra/* $BACKUP_DIR
 
 # comment out deprecated settings from 4.6 cassandra.yaml file
+echo "Upgrading the backed up cassandra.yaml to be 4.8 compatible.."
 sed -i -e "s/multithreaded_compaction: \(.*\)/# multithreaded_compaction: \1/g" $NEW_YAML
 sed -i -e "s/memtable_flush_queue_size: \(.*\)/# memtable_flush_queue_size: \1/g" $NEW_YAML
 sed -i -e "s/compaction_preheat_key_cache: \(.*\)/# compaction_preheat_key_cache: \1/g" $NEW_YAML
@@ -23,42 +25,57 @@ sed -i -e "s/preheat_kernel_page_cache: \(.*\)/# preheat_kernel_page_cache: \1/g
 sed -i -e "s/broadcast_address: \(.*\)/broadcast_rpc_address: \1/g" $NEW_YAML
 
 # update java to version 8
+echo "Installing Java 8"
 add-apt-repository ppa:webupd8team/java -y
 aptitude update -y
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 aptitude install oracle-java8-installer -y
 
 #verify version
+echo "Setting Java 8 to the default version..."
 java -version
 update-java-alternatives -s java-8-oracle
 java -version
 
 #remove v7
+echo "Removing Java 7..."
 aptitude remove oracle-java7-installer oracle-java7-set-default -y
 
 # shutdown dse service cleanly
+
+echo "Flushing cassandra..."
 nodetool flush
+
+echo "Sleeping ${SLEEP_TIME}s..."
 sleep $SLEEP_TIME
+
+echo "Draining cassandra..."
 nodetool drain
+
+echo "Sleeping ${SLEEP_TIME}s..."
 sleep $SLEEP_TIME
+
+echo "Stopping DSE service..."
 service dse stop
 
 # upgrade dse using .deb and aptitude
+echo "Upgrading DSE to 4.8.x..."
 aptitude upgrade dse-full -y
 
 #copy 4.8 yaml back to original location
+echo "Moving 4.8 version of yaml back to etc..."
 cp $NEW_YAML $DEFAULT_YAML_LOCATION
 
 #force ownership of directory
+echo "Reapplying ownership of directories owned by cassandra user..."
 chown -R cassandra:cassandra /mnt/cassandra
 
 # startup dse
+echo "Starting DSE service..."
 service dse start
 
 
 # remaining manual steps
-# 1. verify everything done above looks good
-# 2. start dse service: service dse start
-# 3. check node is back online and seen by other nodes: nodetool status
-# 4. upgrade the existing sstables: nodetool upgradesstables
-# 5. Review logs to ensure all went well
+# 1. check node is back online and seen by other nodes: nodetool status
+# 2. upgrade the existing sstables: nodetool upgradesstables
+# 3. Review logs to ensure all went well
